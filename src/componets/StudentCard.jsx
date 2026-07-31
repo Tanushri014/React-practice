4b845592ce85803e8ae0e00f3eb845633bf7cea2
Tanushri014 <matretanushri0@gmail.com>
2026-07-31 18:31:49 +0530

    student card done

---
+++ src/componets/StudentCard.jsx
@@ -0,0 +1,31 @@
+function StudentCard(){
+    return(
+<div className="outer">
+    <div className="inner">
+<form>
+    <label>Name</label>
+    <br/>
+    <input type="text"/>
+<br/>
+    <label>Branch</label>
+    <br/>
+    <input type="text"/>
+<br/>
+    <label>College</label>
+    <br/>
+    <input type="text"/>
+<br/>
+    <label>cgpa</label>
+    <br/>
+    <input type="number"/>
+    <br/>
+</form>
+    </div>
+</div>
+
+
+
+
+    )
+}
+export default StudentCard;
